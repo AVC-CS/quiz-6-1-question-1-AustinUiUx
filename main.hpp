d1d6c47d2fd0ad1f97d59f7b47917f491bbb766e
AustinUiUx <AustinUiUx@gmail.com>
2024-12-04 12:34:10 -0800

    v1

--- main.hpp
+++ main.hpp
@@ -1,8 +1,40 @@
+#include <iostream>
+#include <cmath>
 
-//********************
-// Declare Function Prototypes
-//********************
+using namespace std;
 
-// ******************************
-// Implement all your functions here
-// ******************************
+// Function prototypes
+void getTwoValues(int& begin, int& end);
+int getNextPrime(int num);
+int getPrevPrime(int num);
+
+
+void getTwoValues(int& begin, int& end) {
+    cout << "Enter two integer values (begin end): ";
+    cin >> begin >> end;
+}
+
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i <= sqrt(num); ++i) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+
+int getNextPrime(int num) {
+    while (true) {
+        ++num;
+        if (isPrime(num)) return num;
+    }
+}
+
+
+int getPrevPrime(int num) {
+    while (true) {
+        --num;
+        if (isPrime(num)) return num;
+    }
+}
